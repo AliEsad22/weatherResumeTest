3517147ec551b0e2b2e4275c7aeb86e9fd98f2d2
Salih Bayram <alicamci2020@gmail.com>
2022-05-04 17:22:50 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useEffect } from "react"
 import { BrowserRouter } from "react-router-dom"
 import CustomRoutes from "./router/CustomRoutes"
 
 function App() {
+  useEffect(() => {
+    return () => {
+      localStorage.removeItem("token")
+      localStorage.removeItem("password")
+    }
+  }, [])
+
   return (
     <BrowserRouter>
       <CustomRoutes />
